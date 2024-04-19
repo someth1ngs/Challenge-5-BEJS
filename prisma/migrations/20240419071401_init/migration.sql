712dc8b56e0dcb1a012543e267079fb61c55d7f4
Dewangga <hamadakinose@gmail.com>
2024-04-19 14:20:04 +0700

    init testing

---
+++ prisma/migrations/20240419071401_init/migration.sql
@@ -0,0 +1,56 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" SERIAL NOT NULL,
+    "identity_type" TEXT NOT NULL,
+    "identity_number" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" SERIAL NOT NULL,
+    "bank_name" TEXT NOT NULL,
+    "bank_account_number" TEXT NOT NULL,
+    "balance" INTEGER NOT NULL,
+    "user_id" INTEGER,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "transaction" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "source_account_id" INTEGER NOT NULL,
+    "destination_account_id" INTEGER NOT NULL,
+
+    CONSTRAINT "transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_user_id_key" ON "profiles"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_source_account_id_fkey" FOREIGN KEY ("source_account_id") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_destination_account_id_fkey" FOREIGN KEY ("destination_account_id") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
