e89546e1d6e7c70a070d40d87a3ac42381649e31
Dewangga <hamadakinose@gmail.com>
2024-04-20 10:02:48 +0700

    add auth controller

---
+++ controller/v1/authController.js
@@ -0,0 +1,92 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = process.env;
+
+module.exports = {
+  register: async (req, res, next) => {
+    try {
+      let { name, email, password } = req.body;
+
+      if (!name || !email || !password) {
+        return res.status(401).json({
+          status: false,
+          message: "input must be required",
+          data: null,
+        });
+      }
+
+      let exist = await prisma.user.findFirst({ where: { email } });
+
+      if (exist) {
+        return res.status(400).json({
+          status: false,
+          message: "email has already been used!",
+          data: null,
+        });
+      }
+
+      let encryptedPassword = await bcrypt.hash(password, 10);
+      let userData = { name, email, password: encryptedPassword };
+      let user = await prisma.user.create({ data: userData });
+      delete user.password;
+
+      return res.status(201).json({
+        status: true,
+        message: "success",
+        data: { user },
+      });
+
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  login: async (req, res, next) => {
+    try {
+      let { email, password } = req.body;
+
+      let user = await prisma.user.findFirst({ where: { email } });
+      if (!user) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid email or password",
+          data: null,
+        });
+      }
+
+      let isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        return res.status(400).json({
+          status: false,
+          message: "Invalid email or password",
+          data: null,
+        });
+      }
+
+      delete user.password;
+      let token = jwt.sign(user, JWT_SECRET_KEY);
+
+      return res.status(200).json({
+        status: true,
+        message: "OK",
+        data: { ...user, token },
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  auth: async (req, res, next) => {
+    try {
+      return res.status(200).json({
+        status: true,
+        message: "OK",
+        data: req.user,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
