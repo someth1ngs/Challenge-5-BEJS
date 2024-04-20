2582800c0fcb404543cdeec88b7862a756771220
Dewangga <hamadakinose@gmail.com>
2024-04-20 10:36:21 +0700

    checkpoint

--- controller/v1/authController.js
+++ controller/v1/authController.js
@@ -80,7 +80,7 @@ module.exports = {
 
   auth: async (req, res, next) => {
     try {
-      return res.status(200).json({
+      res.json({
         status: true,
         message: "OK",
         data: req.user,
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,17 +3,57 @@ const swaggerUI = require("swagger-ui-express");
 const YAML = require("yaml");
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET_KEY } = process.env;
 
+// Restrict for Authenticate
+let restrict = (req, res, next) => {
+  let { authorization } = req.headers;
+  if (!authorization || !authorization.split(" ")[1]) {
+    res.status(401).json({
+      status: false,
+      message: "token not provided",
+      data: null,
+    });
+  }
+
+  let token = authorization.split(" ")[1];
+  let user = jwt.verify(token, JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      res.status(401).json({
+        status: false,
+        message: err.message,
+        data: null,
+      });
+    }
+    delete user.iat;
+    req.user = user;
+    next();
+  });
+
+  console.log("req.user: ", req.user);
+
+  console.log("middleware berjalan dengan baik");
+  next();
+};
+
+// Required Controller
 const accountController = require("../../controller/v1/accountController.js");
 const userController = require("../../controller/v1/userController.js");
 const transactionsController = require("../../controller/v1/transactionsController.js");
+const authController = require("../../controller/v1/authController.js");
 
+// Directory Documentation API
 const swagger_path = path.resolve(__dirname, "../../api-docs.yaml");
 const file = fs.readFileSync(swagger_path, "utf-8");
 
 // API Docs
 const swaggerDocument = YAML.parse(file);
-router.use("/api/v1/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+router.use(
+  "/api/v1/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument)
+);
 
 // API Users + Profile //
 router.post("/api/v1/users", userController.store);
@@ -29,5 +69,10 @@ router.get("/api/v1/accounts/:id", accountController.show);
 router.post("/api/v1/transactions", transactionsController.store);
 router.get("/api/v1/transactions", transactionsController.index);
 router.get("/api/v1/transactions/:id", transactionsController.show);
+
+// API Auth //
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
+router.get("/auth/authenticate", restrict, authController.auth);
 
 module.exports = router;
